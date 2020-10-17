5704d1d5a65dd43c9816049c5822b6b8159b48d8
JesseLopesTI <jesselopes.ti2020@gmail.com>
2020-10-16 21:24:16 -0300

    Renomeando arquivo e definindo script

---
+++ happy-web/src/database/fakedata.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-27.222633",
+    lng: "-49.6455874",
+    name: "Lar das Meninas",
+    description: "Presta assistência a criança de 6 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1576043005963-f4b2a8d1195d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1576043005963-f4b2a8d1195d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horários de visitas das 8h às 18h.",
+    open_on_weekends: "1"
+  },
+  {
+    id: 2,
+    lat: "-27.222633",
+    lng: "-49.6555874",
+    name: "Lar dos Meninos",
+    description: "Presta assistência a criança de 6 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1576043005963-f4b2a8d1195d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1576043005963-f4b2a8d1195d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+    ],
+    instructions: "Venha quando se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horários de visitas das 8h às 18h.",
+    open_on_weekends: "0"
+  },
+]
